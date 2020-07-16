d4edb1a51469972b7313974ea736ab8cbea7759a
Sebastian Lopez <lopez.sebastian@correounivalle.edu.co>
2020-07-16 14:08:33 -0500

    Added DFS and BFS studies and some exercises using jupyter notebook

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,17 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
+
+    if n == 0 or n == 1:
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    lst = [None] * (n + 1)
+    lst[0] = 0
+    lst[1] = 1
+    for i in range(2, n + 1):
+        lst[i] = lst[i-1] + lst[i-2]
+
+    return lst[n]
+
 
 n = int(input())
 print(calc_fib(n))
