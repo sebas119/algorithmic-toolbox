157b232759af1a1d9dcba92f6022fe32c1192f3b
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-07-26 19:31:03 -0500

    Money change problem greedy solution

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,17 @@
 # Uses python3
-import sys
+
 
 def get_change(m):
     #write your code here
-    return m
+
+    ans = 0
+
+    ans += m // 10
+    ans += (m % 10) // 5
+    ans += (m % 10) - (((m % 10) // 5 ) * 5)
+
+    return ans
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
