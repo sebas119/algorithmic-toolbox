30c929aa32dde82b1e5a1e4cc8d7a598644a85eb
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-02 15:39:30 -0500

    Majority element solved with O(N) complexity and O(N) extra space using hash table approach

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,33 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
+""" def get_majority_element(a, left, right):
     if left == right:
         return -1
     if left + 1 == right:
         return a[left]
     #write your code here
-    return -1
+    return -1 """
+
+def get_majority_element_hash_approach(a, n):
+    new = {}
+
+    for e in a:
+        if e not in new:
+            new[e] = 1
+        else:
+            new[e] += 1
+    for keys, val in new.items():
+        if val > n / 2:
+            return 1
+    return 0
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    n = int(input())
+    a = list(map(int, input().split()))
+    # if get_majority_element(a, 0, n) != -1:
+    if get_majority_element_hash_approach(a, n):    
         print(1)
     else:
         print(0)
