d5877c27b2f0813cfe966e72f8b2f9819e510b68
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-02 17:04:23 -0500

    Number of inversions in array using divide and conquer

--- week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -1,18 +1,49 @@
 # Uses python3
-import sys
 
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
+    if left < right:        
+        mid = (left + right) // 2
+        number_of_inversions += get_number_of_inversions(a, b, left, mid)
+        number_of_inversions += get_number_of_inversions(a, b, mid + 1, right)
+        #write your code here
+        number_of_inversions += merge(a, b, left, mid, right)
+    return number_of_inversions
+
+def merge(a, b, left, mid, right):
+    i = left
+    j = mid + 1
+    k = left
+    number_of_inversions = 0
+
+    while i <= mid and j <= right:
+        if a[i] <= a[j]:
+            b[k] = a[i]
+            k += 1
+            i += 1
+        else:
+            b[k] = a[j]
+            number_of_inversions += mid - i + 1
+            k += 1
+            j += 1
+
+    while i <= mid:
+        b[k] = a[i]
+        k += 1
+        i += 1
+
+    while j <= right:
+        b[k] = a[j]
+        k += 1
+        j += 1
+    
+    for l in range(left, right - 1):
+        a[l] = b[l]
+
     return number_of_inversions
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
+    n = int(input())
+    a = list(map(int, input().split()))
     b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    print(get_number_of_inversions(a, b, 0, len(a) - 1))
