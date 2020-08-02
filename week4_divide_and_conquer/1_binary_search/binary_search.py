8500cb714658334d74c6ebd55d7c05848cf8244b
Sebastian L <lopez.sebastian@correounivalle.edu.co>
2020-08-02 15:11:42 -0500

    Implementation of binary search recursively

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,17 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+def binary_search(a, left, right, key):
+    if right < left:
+        return -1
+    mid = int(left + ((right - left) / 2))
+    # print('rigth: {}, left: {}, mid: {}'.format(right, left, mid))
+    if key == a[mid]:
+        return mid
+    elif key < a[mid]:
+        return binary_search(a, left, mid - 1, key)
+    else:
+        return binary_search(a, mid + 1, right, key)
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -12,11 +20,13 @@ def linear_search(a, x):
     return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    
+    data = list(map(int, input().split()))
+    keys = list(map(int, input().split()))
     n = data[0]
-    m = data[n + 1]
     a = data[1 : n + 1]
-    for x in data[n + 2:]:
+    for x in keys[1:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        # print(linear_search(a, x), end = ' ')
+        # print('binary_search({},{},{},{})'.format(a, 0, n-1, x))
+        print(binary_search(a, 0, n - 1, x), end=' ')
